91eacdaa3a31c2956c40ff3460804bd9cd384f2e
Guillermo Carreto <g.carreto@yahoo.es>
2016-03-12 16:55:58 +0100

    Settings

--- js/main.js
+++ js/main.js
@@ -252,7 +252,99 @@ function newQueja(tipo){
 }
 
 function settings(){
-    $('#payLoadSP').html('meter codigo');
+    $('#payLoadSP').html('<div class="row listTarjetasPerf">'+
+
+
+           '<div class=" row well animated pulse tarjeta">'+
+            '<div class="row">'+
+              '<div class="col-xs-12 col-sm-4">'+
+                '<div class="row">'+
+                  '<div class="col-xs-6 col-sm-12">'+
+                    '<img src="https://randomuser.me/api/portraits/med/men/44.jpg" alt="" class="img img-responsive imgTarjeta img-circle">' +
+                  '</div>'
+                  '<div class="col-xs-6  col-sm-12">' +
+                   ' <h2>change image</h2>'+
+                  '</div>'+
+                '</div>'+
+              '</div>'+
+              '<div class="col-xs-12 col-sm-8">'+
+                '<div class="row">'+
+                 '<div class="col-xs-12 text-center">'+
+                  '<h3>Información</h3>'+
+                '</div>'+
+
+                '<div class="col-xs-12">'+
+                  '<form autocomplete="off" class="form-horizontal" role="form">'+
+                    '<div class="form-group">'+
+                      '<label class="control-label col-sm-2" for="alias">Alias:</label>'+
+                      '<div class="col-sm-10">'+
+                        '<input type="text" autocomplete="off" class="form-control" id="alias" placeholder="@JuanitoTaxiCrazy">'+
+                      '</div>'+
+                    '</div>'+
+                    '<div class="form-group">'+
+                      '<label class="control-label col-sm-2" for="bio">Bio:</label>'+
+                      '<div class="col-sm-10">'+
+                        '<textarea class="form-control" rows="4"  placeholder="Conduzco mi Taxi mientras escucho los partidos del equipo de mi barrio por la radio!" ></textarea>'+
+                      '</div>'+
+                    '</div>'+
+                    '<div class="form-group">'+
+                     '<label class="control-label col-sm-2" for="mail">Email:</label>'+
+                      '<div class="col-sm-10">'+
+                       '<input type="mail" autocomplete="off" class="form-control" id="mail" placeholder="taxiCrazy@lepeTAxi.lp">'+
+                     '</div>'+
+                   '</div>'+
+
+                   '<hr>'+
+                   '<div class="col-xs-12 text-center">'+
+                    '<h3>Notificaciones</h3>'+
+                  '</div>'+
+                  '<div class="form-group">'+
+                    '<div class="col-xs-4 col-xs-offset-4">'+
+                     '<input type="checkbox" data-toggle="toggle" data-on="activas" data-off="inactivas" id="not" data-onstyle="success" data-offstyle="danger">'+
+                  '</div>'+
+                 '</div>'+
+                 '<hr>'+
+                 '<div class="col-xs-12 text-center">'+
+                  '<h3>Seguridad</h3>'+
+                '</div>'+
+
+
+                '<div class="form-group text-center">'+
+                  '<label class="control-label col-xs-12  col-sm-5  col-md-6 text-center" for="pass1">Password:</label>'+
+                  '<div class="col-xs-12 col-sm-7 col-md-6">'+
+                   '<input type="password" autocomplete="off" class="form-control" id="pass1" placeholder="">'+
+                 '</div>'+
+               '</div>'+
+               '<div class="form-group">'+
+                '<label class="control-label col-xs-12 col-sm-5 col-md-6 text-center" for="pass2">Repite password:</label>'+
+                '<div class="col-xs-12 col-sm-7 col-md-6">'+
+                 '<input type="password" autocomplete="off" class="form-control" id="pass2" placeholder="">'+
+               '</div>'+
+             '</div>'+
+
+             '<br>'+
+
+             '<div class="form-group">'+
+              '<div class="col-xs-2 col-xs-offset-2">'+
+
+                '<button type="submit" class="btn btn-default btn-valid" onclick="profile();">Guardar</button>'+
+              '</div>'+
+              '<div class="col-xs-2 col-xs-offset-2">'+
+
+                '<button type="submit" class="btn btn-default  btn-cancel" onclick="profile();">Cancelar</button>'+
+              '</div>'+
+            '</div>'+
+          '</form>'+
+
+        '</div>'+
+      '</div>'+
+    '</div>'+
+
+  '</div>'+
+  
+
+'</div>'+
+'</div>');
 
 
 
